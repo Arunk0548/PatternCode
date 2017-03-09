c49c27139d982a939375aafa90b4fc4828b863ce
Arunk0548 <Arunk0548@users.noreply.github.com>
2017-03-09 15:25:51 -0500

    update source code

--- src/P1/Pattern.java
+++ src/P1/Pattern.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Pattern {
    public static void main(String args[] ) throws Exception {
-
+	System.out.println("Enter an integer number n [where N>=1, N<=26]");
 	Scanner s = new Scanner(System.in);
     int inputNum = Integer.parseInt(s.nextLine());
 	int num = 0;
--- src/P1/README.md
+++ src/P1/README.md
@@ -1,2 +1,17 @@
 # PatternCode
 Java Pattern Programs
+
+
+~~~
+Take an integer input and draw the below pattern. Say for example drawn below pattern for input number 5.
+
+        E
+      E D
+    E D C
+  E D C B
+E D C B A
+  E D C B
+    E D C
+      E D
+        E
+~~~
