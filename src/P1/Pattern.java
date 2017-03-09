539be34686ac8f1c5855e26cb7c92b5aa7fa4b57
Arunk0548 <Arunk0548@users.noreply.github.com>
2017-03-09 15:29:04 -0500

    formatting the code

--- src/P1/Pattern.java
+++ src/P1/Pattern.java
@@ -1,33 +1,33 @@
 import java.io.*;
 import java.util.*;
 public class Pattern {
-   public static void main(String args[] ) throws Exception {
-	System.out.println("Enter an integer number n [where N>=1, N<=26]");
-	Scanner s = new Scanner(System.in);
-    int inputNum = Integer.parseInt(s.nextLine());
-	int num = 0;
-    while(num<inputNum)
-    {
-        for(int index =0; index<inputNum - num-1;index++)
-            System.out.print("  ");
-        char printChar = (char)(64+inputNum);
-        for(int index = 0;index<num; index++,printChar--)
-            System.out.print(printChar + " ");
-        
-        System.out.println(printChar);
-		num++;
-    }
-    while(num>1)
-    {
-        for(int index =0; index<inputNum - num+1;index++)
-            System.out.print("  ");
-        char printChar = (char)(64+inputNum);
-        for(int index = 1;index<num-1; index++,printChar--)
-            System.out.print(printChar + " ");
-        
-        System.out.println(printChar);
-		num--;
-    }
+	public static void main(String args[] ) throws Exception {
+		System.out.println("Enter an integer number n [where N>=1, N<=26]");
+		Scanner s = new Scanner(System.in);
+		int inputNum = Integer.parseInt(s.nextLine());
+		int num = 0;
+		while(num<inputNum)
+		{
+			for(int index =0; index<inputNum - num-1;index++)
+			System.out.print("  ");
+			char printChar = (char)(64+inputNum);
+			for(int index = 0;index<num; index++,printChar--)
+			System.out.print(printChar + " ");
+			
+			System.out.println(printChar);
+			num++;
+		}
+		while(num>1)
+		{
+			for(int index =0; index<inputNum - num+1;index++)
+			System.out.print("  ");
+			char printChar = (char)(64+inputNum);
+			for(int index = 1;index<num-1; index++,printChar--)
+			System.out.print(printChar + " ");
+			
+			System.out.println(printChar);
+			num--;
+		}
 
-   }
+	}
 }
